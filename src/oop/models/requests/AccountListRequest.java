985f9dddccccb1e0fb594c1f322ed71c3f880e12
sam <kayodesamson4@gmail.com>
2025-09-02 22:31:26 +0100

    Add account list request class

---
+++ src/oop/models/requests/AccountListRequest.java
@@ -0,0 +1,37 @@
+package oop.models.requests;
+
+public class AccountListRequest {
+    private String email;
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public static String validate(AccountListRequest userLoginRequest){
+        if(userLoginRequest == null){
+            return "User login request cannot be null";
+        }
+        if(userLoginRequest.getPassword() == null ||userLoginRequest.getPassword().isEmpty()){
+            return "Password cannot be null or empty";
+        }
+        if(userLoginRequest.getEmail() == null ||userLoginRequest.getEmail().isEmpty()){
+            return "Email cannot be null or empty";
+        }
+
+        return "User login request okay!";
+    }
+}
